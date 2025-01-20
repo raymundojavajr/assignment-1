4bee3468d25a9aadb6b96b197c659df4e81c6052
Jeng Java <raymundojavajr@gmail.com>
2025-01-21 04:53:46 +0800

    add dagster pipelines and repository

--- src/dagster_project/pipeline.py
+++ src/dagster_project/pipeline.py
@@ -1,9 +1,42 @@
-from dagster import job, op
+from dagster import op, job
+from src.data_loading import load_data
+from src.models import train_model
+from src.utils.save_utils import save_model, load_model
+
 
 @op
-def hello_op():
-    return "Hello, Dagster!"
+def load_data_op():
+    file_path = "data/match_maker.xlsx"
+    return load_data(file_path)
+
+
+@op
+def train_model_op(data):
+    x_data = data.drop("target", axis=1)
+    y_data = data["target"]
+    model = train_model(x_data, y_data)
+    return model
+
+
+@op
+def save_model_op(model):
+    file_path = "models/model.pkl"
+    save_model(model, file_path)
+
+
+@op
+def load_model_op():
+    file_path = "models/model.pkl"
+    return load_model(file_path)
+
 
 @job
-def my_pipeline():
-    hello_op()
+def ml_pipeline():
+    data = load_data_op()
+    model = train_model_op(data)
+    save_model_op(model)
+
+
+@job
+def model_evaluation_pipeline():
+    load_model_op()
--- src/dagster_project/repository.py
+++ src/dagster_project/repository.py
@@ -1,6 +1,6 @@
 from dagster import repository
-from src.dagster_project.pipeline import my_pipeline
+from src.dagster_project.pipeline import ml_pipeline, model_evaluation_pipeline
 
 @repository
 def my_repository():
-    return [my_pipeline]
+    return [ml_pipeline, model_evaluation_pipeline]
